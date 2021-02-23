861756f7b46da7d00c51034e5831dc1fed1798ad
maliyalcin08 <maliyalcin08@gmail.com>
2021-02-23 12:58:49 +0300

    Add project files.

---
+++ GameProject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using GameProject.Concrete;
+using GameProject.Entities;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            Gamer gamer = new Gamer
+            {
+                Id = 1,
+                FirstName = "Muhammet Ali",
+                LastName = "Yalçın",
+                IdentityNumber = 22567312266,
+                DateOfBirth = new DateTime(1997,6,11)
+            };
+            gamerManager.Add(gamer);
+
+            CampaignRateManager campaignRateManager = new CampaignRateManager();
+            Campaign campaign = new Campaign
+            {
+                CampaignId = 2,
+                CampaignName = "Abduction. %40 OFF!",
+                DiscountRate = 0.40M
+            };
+            campaignRateManager.Add(campaign);
+
+            GameManager gameManager = new GameManager();
+            Game game = new Game
+            {
+                GameId = 3,
+                GameName = "HelloWorld",
+                GamePrice = 450
+            };
+            gameManager.Add(game);
+
+            SalesManager salesManager = new SalesManager();
+            salesManager.Sales(gamer, game);
+            salesManager.CampaignSales(gamer, game, campaign);
+        }
+    }
+}
